78f73e31a2c4e1dbfb25ef5dc8ed18e8b18fe58f
“Brayan <xxbrayan@outlook.com>
2022-06-06 21:19:40 -0500

    Primera Version APPSALON

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'logarto/app-salon_php_mvc_js_sass',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'logarto/app-salon_php_mvc_js_sass' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
